ad87c2d2e1e969031a3c1f7efabd78f3c644cf43
Хван Александр <zillendaw@stud.vvsu.ru>
2025-10-10 12:25:37 +1000

    Третье задание для lab3

---
+++ labs/lab3/task_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <locale.h>
+using namespace std;
+
+int main() 
+{
+    for (int i = 0; i <= 100; i+= 3)
+    {
+        cout << i << endl;
+    }
+}
