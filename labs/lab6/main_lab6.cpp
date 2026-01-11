37e320145b419f9f98614a3ad9edf9df3b07e5e6
Sanyek004 <sasha88543@gmail.com>
2026-01-11 19:27:14 +1000

    add task_2 in lab_6

--- labs/lab6/main_lab6.cpp
+++ labs/lab6/main_lab6.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <string>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 double task_1(double num, char type_now, char type_translate);
+double task_2(double sum, double age);
 
 double task_1(double num, char type_now, char type_translate)
 {
@@ -27,12 +27,64 @@ double task_1(double num, char type_now, char type_translate)
     }
 }
 
+double task_2(double sum, double age)
+{
+    double sum_bonus = 0;
+
+    if (sum < 30000)
+    {
+        cout << "Minimum deposit: 30k!";
+        return 0;
+    }
+
+    if (sum > 10000)
+    {
+        // (int) отбрасывает дробную часть, чтобы считать полные десятки тысяч
+        sum_bonus = (int)(sum / 10000) * 0.3;
+
+        if (sum_bonus > 5.0)
+        {
+            sum_bonus = 5.0;
+        }
+
+    }
+
+    double current_sum = sum;
+
+    for (int i = 1; i <= age; i++)
+    {
+        double current_year_rate = sum_bonus; 
+
+        
+        if (i <= 3) 
+        {
+            current_year_rate += 3.0;
+        }
+        else if (i >= 4 && i <= 6) 
+        {
+            current_year_rate += 5.0;
+        }
+        else 
+        {
+            current_year_rate += 2.0;
+        }
+
+        current_sum += current_sum * (current_year_rate / 100.0);
+
+        current_sum = round(current_sum * 100) / 100.0;
+    }
+
+    return current_sum - sum;
+}
+
 
 int main()
 {
     double res_1 = task_1(12, 's', 'm');
+    double res_2 = task_2(200000, 8);
 
     cout << "Result TASK_1: " << res_1 << endl;
+    cout << "Result TASK_2: " << fixed << setprecision(2) << res_2 << endl;
 
     return 0;
 }
