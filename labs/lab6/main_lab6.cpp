3d8a7a3fa661543fe5a6c103e07eac1a4d973752
Sanyek004 <sasha88543@gmail.com>
2026-01-20 17:40:02 +1000

    add task_3 in lab_6

--- labs/lab6/main_lab6.cpp
+++ labs/lab6/main_lab6.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <vector>
 using namespace std;
 
 double task_1(double num, char type_now, char type_translate);
 double task_2(double sum, double age);
+vector<unsigned int> task_3(int begin, int end);
 
 double task_1(double num, char type_now, char type_translate)
 {
@@ -77,11 +79,56 @@ double task_2(double sum, double age)
     return current_sum - sum;
 }
 
+vector<unsigned int> task_3(int begin, int end)
+{
+    vector<unsigned int> task_3;
+
+    if (end < 2) 
+    {
+        cout << "Error!";
+        return task_3;
+    }
+
+    if (begin < 2) {
+        begin = 2;
+    }
+
+    for (int n = begin; n <= end; ++n) 
+    {
+        bool is_prime = true;
+        
+        for (int i = 2; i * i <= n; ++i) 
+        {
+            if (n % i == 0) 
+            {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if (is_prime) 
+        {
+            task_3.push_back(n);
+        }
+    }
+
+
+    cout << "Simple numbers: ";
+    for (auto it = task_3.begin(); it != task_3.end(); it++)
+    {
+        cout << " " << *it << " ";
+    }
+
+    return task_3;
+
+}
+
 
 int main()
 {
     double res_1 = task_1(12, 's', 'm');
     double res_2 = task_2(200000, 8);
+    task_3(0,1);
 
     cout << "Result TASK_1: " << res_1 << endl;
     cout << "Result TASK_2: " << fixed << setprecision(2) << res_2 << endl;
